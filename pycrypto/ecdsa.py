8b07bd326e0ccb06ed6a4a6e0faeffcf936f894a
OnedgeLee <onedge.lee@gmail.com>
2022-05-10 12:43:56 +0900

    🤡 Mock methods related to signature features

--- pycrypto/ecdsa.py
+++ pycrypto/ecdsa.py
@@ -2,6 +2,8 @@
 """
 
 from secrets import randbelow
+from types import NoneType
+from typing import Union
 
 from pycrypto.libs.ec_types import Secp256k1
 from pycrypto.libs.ec_types import secp256k1
@@ -50,4 +52,96 @@ class ECDSA:
         Returns:
             str: Public key hex string
         """
+        private_key_int = int(private_key, 16)  # Doesn't need explicit exception
+
+        if not 1 <= private_key_int <= self.ec_type.p - 1:
+            raise ValueError("Private key is not in the proper range")
+
         return hex((self.gen_point * int(private_key, 16)).x)
+
+    def keygen(self, private_key: Union[NoneType, str] = None) -> str:
+        """Key pair generation
+
+        Args:
+            private_key (Union[NoneType, str], optional): If private key is specified,
+            it will work just as method 'keygen_public',
+            or it will generate new private key. Defaults to None.
+
+        Returns:
+            tuple: tuple containing:
+
+                private_key (str): Private key hex string
+                public_key (str): Public key hex string
+        """
+        if private_key is None:
+            private_key = self.keygen_private()
+        return private_key, self.keygen_public(private_key)
+
+    def gen_message_hash(self, message: str) -> str:
+        """Message hash generation with given message and SHA-2 algorithm
+        Its first proposal was to use SHA-1, but it has some vulnerabilities,
+        Adopting SHA-2 would be better. (Chose SHA-256)
+
+        Args:
+            message (str): Message to be hashed
+
+        Returns:
+            str: Message hash hex string (256 bit)
+        """
+        # TODO: To be implemented
+        pass
+
+    def gen_signature(self, private_key: str, message: str) -> str:
+        """Signature generation with given private key and message
+        d : private key
+        Q : public key
+        G : generator point
+
+        1. Generate k (random number)
+        2. Generate r (Px where P = k * G)
+        3. Generate z (message hash)
+        4. Generate s (s = k^-1 * (z + d * r) mod p)
+        5. Generate signature with DER(r, s)
+
+        DER :
+            1. 0x30 byte: header byte to indicate compound structure
+            2. one byte to encode the length of the following data
+            3. 0x02: header byte indicating an integer
+            4. one byte to encode the length of the following r value
+            5. the r value as a big-endian integer
+            6. 0x02: header byte indicating an integer
+            7. one byte to encode the length of the following s value
+            8. the s value as a big-endian integer
+
+        Args:
+            private_key (str): Private key hex string
+            message (str): Message string
+
+        Returns:
+            str : Signature
+        """
+        # TODO: To be implemented
+        pass
+
+    def validate(self, message: str, signature: str, public_key: str) -> bool:
+        """Message validation
+        Q : public key
+        G : generator point
+        z : message hash
+        r : signature r
+        s : signature s
+
+        1. Parse signature (r, s) = DECODE(signature)
+        2. Generate z(message hash) from message z = SHA-256(message)
+        3. Compute r == Px where P = s^-1 * z * G + s^-1 * r * Q
+
+        Args:
+            message (str): Message
+            signature (str): Signature
+            public_key (str): Public key
+
+        Returns:
+            bool: If valid return True, else return False
+        """
+        # TODO: To be implemented
+        pass
